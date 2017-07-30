c4251f06051f12b869e9599a6ae6188c17bde16f
Matt Simerson <matt@tnpi.net>
2017-07-30 16:20:12 -0700

    more path shortening

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const constants = require('haraka-constants');
 exports.register = function () {
   var plugin = this;
 
-  plugin.Syslog = require('modern-syslog');
+  const syslog = require('modern-syslog');
+  plugin.Syslog = syslog;
 
   var options   = 0;
   plugin.load_syslog_ini();
@@ -17,63 +18,63 @@ exports.register = function () {
 
   ['pid','odelay','cons','ndelay','nowait'].forEach(opt => {
     if (!plugin.cfg.general[opt]) return;
-    options |= plugin.Syslog['LOG_' + opt.toUpperCase() ];
+    options |= syslog['LOG_' + opt.toUpperCase() ];
   })
 
   switch (facility.toUpperCase()) {
     case 'MAIL':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_MAIL);
+      syslog.init(name, options, syslog.LOG_MAIL);
       break;
     case 'KERN':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_KERN);
+      syslog.init(name, options, syslog.LOG_KERN);
       break;
     case 'USER':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_USER);
+      syslog.init(name, options, syslog.LOG_USER);
       break;
     case 'DAEMON':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_DAEMON);
+      syslog.init(name, options, syslog.LOG_DAEMON);
       break;
     case 'AUTH':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_AUTH);
+      syslog.init(name, options, syslog.LOG_AUTH);
       break;
     case 'SYSLOG':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_SYSLOG);
+      syslog.init(name, options, syslog.LOG_SYSLOG);
       break;
     case 'LPR':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LPR);
+      syslog.init(name, options, syslog.LOG_LPR);
       break;
     case 'NEWS':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_NEWS);
+      syslog.init(name, options, syslog.LOG_NEWS);
       break;
     case 'UUCP':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_UUCP);
+      syslog.init(name, options, syslog.LOG_UUCP);
       break;
     case 'LOCAL0':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LOCAL0);
+      syslog.init(name, options, syslog.LOG_LOCAL0);
       break;
     case 'LOCAL1':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LOCAL1);
+      syslog.init(name, options, syslog.LOG_LOCAL1);
       break;
     case 'LOCAL2':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LOCAL2);
+      syslog.init(name, options, syslog.LOG_LOCAL2);
       break;
     case 'LOCAL3':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LOCAL3);
+      syslog.init(name, options, syslog.LOG_LOCAL3);
       break;
     case 'LOCAL4':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LOCAL4);
+      syslog.init(name, options, syslog.LOG_LOCAL4);
       break;
     case 'LOCAL5':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LOCAL5);
+      syslog.init(name, options, syslog.LOG_LOCAL5);
       break;
     case 'LOCAL6':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LOCAL6);
+      syslog.init(name, options, syslog.LOG_LOCAL6);
       break;
     case 'LOCAL7':
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_LOCAL7);
+      syslog.init(name, options, syslog.LOG_LOCAL7);
       break;
     default:
-      plugin.Syslog.init(name, options, plugin.Syslog.LOG_MAIL);
+      syslog.init(name, options, syslog.LOG_MAIL);
   }
 
   plugin.register_hook('log', 'syslog');
@@ -103,33 +104,33 @@ exports.syslog = function (next, logger, log) {
 
   switch (log.level.toUpperCase()) {
     case 'INFO':
-      Syslog.log(Syslog.LOG_INFO, log.data);
+      syslog.log(syslog.LOG_INFO, log.data);
       break;
     case 'NOTICE':
-      Syslog.log(Syslog.LOG_NOTICE, log.data);
+      syslog.log(syslog.LOG_NOTICE, log.data);
       break;
     case 'WARN':
-      Syslog.log(Syslog.LOG_WARNING, log.data);
+      syslog.log(syslog.LOG_WARNING, log.data);
       break;
     case 'ERROR':
-      Syslog.log(Syslog.LOG_ERR, log.data);
+      syslog.log(syslog.LOG_ERR, log.data);
       break;
     case 'CRIT':
-      Syslog.log(Syslog.LOG_CRIT, log.data);
+      syslog.log(syslog.LOG_CRIT, log.data);
       break;
     case 'ALERT':
-      Syslog.log(Syslog.LOG_ALERT, log.data);
+      syslog.log(syslog.LOG_ALERT, log.data);
       break;
     case 'EMERG':
-      Syslog.log(Syslog.LOG_EMERG, log.data);
+      syslog.log(syslog.LOG_EMERG, log.data);
       break;
     case 'DATA':
     case 'PROTOCOL':
     case 'DEBUG':
-      Syslog.log(Syslog.LOG_DEBUG, log.data);
+      syslog.log(syslog.LOG_DEBUG, log.data);
       break;
     default:
-      Syslog.log(Syslog.LOG_DEBUG, log.data);
+      syslog.log(syslog.LOG_DEBUG, log.data);
   }
 
   if (plugin.cfg.always_ok) {
